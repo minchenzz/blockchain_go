4d37448fe853f75a27a2850df684ef27d5483c29
minchenzz <rimutuyuan56@gmail.com>
2019-12-11 12:28:46 +0800

    update

---
+++ t.py
@@ -0,0 +1,24 @@
+import datetime
+import random
+import os
+import time
+
+end = datetime.datetime.now()
+start = end - datetime.timedelta(days=364)
+
+def do(dt):
+    if not random.randint(0, 5):
+        for i in range(random.randint(0,2)):
+            with open('commit.log', 'a+') as f:
+                f.write(str(dt) +'\n')
+            os.system('git add .')
+            print(f"{str(dt)[:-9]}{random.randint(0,59)}", i)
+            os.system(f'git commit -a -m "update" --date="{str(dt)[:-9]}{random.randint(10,59)}"')
+
+
+
+while start < end:
+    start = start + datetime.timedelta(days=1)
+    do(start)
+
+
